7a5d0ac0f71c3f9c5e1ea65fc6f6b612dfb5c81b
Patrick Mutua <patrickmutua719@gmail.com>
2025-08-23 22:01:48 +0300

    First commit

---
+++ src/Pages/About_me.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About_me = () => {
+  return <div>About_me</div>;
+};
+
+export default About_me;
---
+++ src/Pages/Home.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <section className="home-wrapper">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div>
+                <h4 className="typing-text">
+                  My name is Patrick Mutua and I am an experienced software
+                  developer.
+                  <br />I build clean, scalable, and reliable software
+                  solutions.
+                </h4>
+                <div>
+                  <img
+                    src="https://th.bing.com/th/id/OIP.StYwWSD2f02rdx58xWRExQHaEK?w=274&h=180&c=7&r=0&o=7&pid=1.7&rm=3"
+                    alt="profile"
+                  />
+                </div>
+                <div>
+                  <a href="#projects" className="btn-link">
+                    Projects
+                  </a>
+                </div>
+                <div>
+                  <a href="#contact" className="btn-link">
+                    Contact
+                  </a>
+                </div>
+                <div>
+                  <a href="#aboutme" className="btn-link">
+                    About me
+                  </a>
+                </div>
+                <div className="tech-stack">
+                  <p>Tech stack:</p>
+                  <img src="images/Mongodb.png" alt="mongoosedb" />
+                  <img src="images/express js.png" alt="express js" />
+                  <img src="images/react.jpg" alt="react" />
+                  <img src="images/images.png" alt="node" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer className="site-footer">
+          &copy;{new Date().getFullYear()} Patrick .M. Mutua
+        </footer>
+      </section>
+    </>
+  );
+};
+
+export default Home;
